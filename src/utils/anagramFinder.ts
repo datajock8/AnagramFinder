167f172b0da2a9568d0ece906e2639295fad209a
Mike Kwak <datajock8@gmail.com>
2023-08-07 19:41:30 -0700

    Updated with validation input and added comments

--- src/utils/anagramFinder.ts
+++ src/utils/anagramFinder.ts
@@ -1,14 +1,30 @@
-function anagramFinder(input, WORDS) {
-    let possibleWords = new Set();
+/**
+ * Finds all valid English words that can be created using some combination
+ * of the letters in the given input string.
+ * @param input - The input string.
+ * @param WORDS - The array of valid English words.
+ * @returns The array of possible words.
+ */
+function getWords(input: string, WORDS: string[]): string[] {
+    
+    // Input validation to ensure it's a string
+    if (typeof input !== 'string' || !Array.isArray(WORDS)) {
+        throw new Error('Invalid input');
+    }
+    
+    let possibleWords = new Set<string>();
+    
+    // Putting into a set instead of array to improve lookup speed
+    const wordSet = new Set(WORDS);
 
-    function generatePermutations(str, prefix="") {
+    function generatePermutations(str: string, prefix: string = ""): void {
         if (str.length <= 1) {
             let word = prefix + str;
-            if (WORDS.includes(word)) {
+            if (wordSet.has(word)) {
                 possibleWords.add(word);
             }
         } else {
-            for (let i=0; i<str.length; i++) {
+            for (let i = 0; i < str.length; i++) {
                 let cur = str.substring(i, i + 1);
                 let before = str.substring(0, i);
                 let after = str.substring(i + 1);
