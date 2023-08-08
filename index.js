59c19a9b9b5d1e8e812d1c2992569d88e089e588
Mike Kwak <datajock8@gmail.com>
2023-08-07 20:53:24 -0700

    compiled ts for modules

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.anagramFinder = void 0;
+var anagramFinder_1 = require("./utils/anagramFinder");
+Object.defineProperty(exports, "anagramFinder", { enumerable: true, get: function () { return anagramFinder_1.anagramFinder; } });
